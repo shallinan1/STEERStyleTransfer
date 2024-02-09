e7dd657d95b9d1bc2d78eebd073ad18ad1fe20a0
shallinan1 <hallisky@uw.edu>
2024-02-09 13:07:57 -0800

    Add constants

---
+++ utils/constants.py
@@ -0,0 +1,21 @@
+"""
+Stores constants used in other files
+"""
+
+# Store approximation for negative infinity to use in masking
+NEGATIVE_INF = -100000.0 
+
+# Store mapping of shorthand style names in CDS to full names
+STYLE_MAPPING = {
+    "aae": "AAE",
+    "bible": "BIBLE",
+    "coha_1810": "HISTORY_1",
+    "coha_1890": "HISTORY_2",
+    "coha_1990": "HISTORY_3",
+    "joyce": "JOYCE",
+    "lyrics": "LYRICS",
+    "romantic_poetry": "POETRY",
+    "shakespeare": "SHAKESPEARE",
+    "switchboard": "SWITCHBOARD",
+    "english_tweet": "ENG_TWT"
+}
